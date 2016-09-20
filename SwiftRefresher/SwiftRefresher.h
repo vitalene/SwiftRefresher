d36d60a37d2d916d8ba59d0e79f3b214495440f7
Neil <vitalene@msu.edu>
2016-09-20 17:03:06 -0400

    Initial Commit

---
+++ SwiftRefresher/SwiftRefresher.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRefresher.h
+//  SwiftRefresher
+//
+//  Created by Neil Vitale on 9/20/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRefresher.
+FOUNDATION_EXPORT double SwiftRefresherVersionNumber;
+
+//! Project version string for SwiftRefresher.
+FOUNDATION_EXPORT const unsigned char SwiftRefresherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRefresher/PublicHeader.h>
+
+
